0d5edb9e4f16bccd0961cbb27fe66cea555fc5b2
jcarlsontcsp <1958937-jcarlsontcsp@users.noreply.replit.com>
2023-11-02 20:36:38 +0000

    Added code from smooth-scroll to main

--- main.py
+++ main.py
@@ -35,7 +35,9 @@ for y in range(0,50):
     row.append(random.choice(terrain))
   map.append(row)
 
-#print(map)
+#print(map[0][0])
+
+##print(map)
 
 ## initialize pygame and create window
 pygame.init()
@@ -44,12 +46,54 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Monster Battling Game 3")
 clock = pygame.time.Clock()     ## For syncing the FPS
 
-player_x = 55
-player_y = 54
+directions = {
+  'up': (0,1),
+  'down': (0,-1),
+  'left': (1,0),
+  'right': (-1,0),
+  '':(0,0)
+}
+
+offset = [0,0]
+
+player_x = 49
+player_y = 49
+
+SCROLL_TICK = -1
+DIRECTION = ''
+
+def scroll(direction):
+  global offset
+  global SCROLL_TICK
+  global player_x
+  global player_y
+  global DIRECTION
+  if (SCROLL_TICK!=-1):
+    SCROLL_TICK+=1
+    if (SCROLL_TICK!=17):
+      if direction == 'up':
+        offset[1]-=2
+      if direction == 'down':
+        offset[1]+=2
+      if direction == 'left':
+        offset[0]-=2
+      if direction == 'right':
+        offset[0]+=2
+    else:
+      SCROLL_TICK=-1
+      player_x+=int(offset[0]/32)
+      player_y+=int(offset[1]/32)
+      offset = [0,0]
+      #print(player_x, player_y)
+      DIRECTION = ''
+
 
 ## Game loop
 
+
+
 #13 x 11
+#15 X 13 - y: 7 x: 6
 running = True
 
 while running:
@@ -58,34 +102,51 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                player_y += 1
-            if event.key == pygame.K_DOWN:
-                player_y -= 1
-            if event.key == pygame.K_LEFT:
-                player_x += 1
-            if event.key == pygame.K_RIGHT:
-                player_x -= 1
+            if SCROLL_TICK == -1:
+                if event.key == pygame.K_UP:
+                    DIRECTION = 'up'
+                    SCROLL_TICK=0
+                if event.key == pygame.K_DOWN:
+                    DIRECTION = 'down'
+                    SCROLL_TICK=0
+                if event.key == pygame.K_LEFT:
+                    DIRECTION = 'left'
+                    SCROLL_TICK=0
+                if event.key == pygame.K_RIGHT:
+                    DIRECTION = 'right'
+                    SCROLL_TICK=0
+            ##print(directions[DIRECTION][0])
             try:
-                print(map[player_y-5][player_x-6])
+                #print(map[player_y-5][player_x-6])
+                pass
             except:
                 pass
 
-    #print(player_x,"and",y)
+    ##print(player_x,"and",y)
+
+    scroll(DIRECTION)
 
     screen.fill(WHITE)
 
-    for y in range(0,player_y+5):
-        for x in range(0,player_x+6):
-            
-            try:
-                screen.blit(map[player_y-y][player_x-x], (x*32, y*32))
-            except:
-                screen.blit(water, (x*32, y*32))
+    #0,0 should be top left corner
+    # Moving right shifts the map to left
+    # Moving down shifts the map to up
+
+    for y in range(-1, 12):
+      for x in range(-1,14):
+        print(player_x,player_y)
+        if player_x+x-6>=0 and player_y+y-5>=0:
+          try:
+            screen.blit(map[player_y+y-5][player_x+x-6], (x*32-offset[0], y*32-offset[1]))
+          except:
+            screen.blit(water, (x*32-offset[0],y*32-offset[1]))
+        else:
+          screen.blit(water, (x*32-offset[0],y*32-offset[1]))
+          #print('hi')
 
     screen.blit(player, (192,160))
     #screen.blit(grass, (0,0))
-  
+
     pygame.display.flip()       
 
 pygame.quit()
