7a155af53d9bf0dbe107fb64cc2430e13368d120
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2023-11-07 01:20:13 +0000

    Added support for changing scenes quickly and added collisions. Number ids are temporary and the portal does not work currently.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import json
 import sys
 
-import map
+import map as maps
 
 WIDTH = 416
 HEIGHT = 352
@@ -36,20 +36,45 @@ player = pygame.image.load(f'textures/{tPack}/player/player_down.png')
 grass = pygame.image.load(f'textures/{tPack}/terrain/grass.png')
 stone = pygame.image.load(f'textures/{tPack}/terrain/stone.png')
 water = pygame.image.load(f'textures/{tPack}/terrain/water.png')
+shore_down = pygame.image.load(f'textures/{tPack}/terrain/shore_south.png')
+tree = pygame.image.load(f'textures/{tPack}/terrain/tree.png')
+portal = pygame.image.load(f'textures/{tPack}/terrain/portal.png')
 
 grass = pygame.transform.scale(grass, (32, 32))
+
+# Function to draw text on the screen
+def text(msg, x, y, color, size, font): 
+  try:
+    fontobj = pygame.font.SysFont(font, size)
+  except:
+    fontobj = pygame.font.SysFont("freesans", 36)
+  msgobj = fontobj.render(msg, False, color)
+  screen.blit(msgobj,(x, y))
 
 solid_blocks = [grass, stone, water]
 
 map = []
 
 terrain = [grass, grass, stone]
+solids = [-1,1,6]
+
+
+tileIDs = {
+  -1:water,
+  0:grass,
+  1:stone,
+  2:shore_down,
+  6:tree,
+  99:portal
+}
 
 for y in range(0,50):
   row = []
   for x in range(0,50):
     row.append(random.choice(terrain))
   map.append(row)
+
+map = maps.scenes['dev']
 
 #print(map[0][0])
 
@@ -63,20 +88,44 @@ pygame.display.set_caption("Monster Battling Game 3")
 clock = pygame.time.Clock()     ## For syncing the FPS
 
 directions = {
-  'up': (0,1),
-  'down': (0,-1),
-  'left': (1,0),
-  'right': (-1,0),
+  'up': (0,-1),
+  'down': (0,1),
+  'left': (-1,0),
+  'right': (1,0),
   '':(0,0)
 }
 
 offset = [0,0]
 
 player_x = 0
-player_y = 49
+player_y = 0
 
 SCROLL_TICK = -1
 DIRECTION = ''
+
+def playerCollisions(direction):
+  global player_x, player_y, map
+  
+  direction=directions[direction]
+  #print('dir:',direction)
+  
+  if player_x+direction[0]>=0 and player_y+direction[1]>=0:
+    try:
+      tile=map[player_y+direction[1]][player_x+direction[0]]
+    except:
+      return False
+    else:
+    
+      if isinstance(tile, list):
+        if tile[0] in solids:
+          return False
+        return True
+      else:
+        if tile in solids:
+          return False
+        return True
+    return False
+      
 
 def scroll(direction):
   global offset
@@ -120,17 +169,23 @@ while running:
         if event.type == pygame.KEYDOWN:
             if SCROLL_TICK == -1:
                 if event.key == pygame.K_UP:
-                    DIRECTION = 'up'
-                    SCROLL_TICK=0
+                    if playerCollisions('up'):
+                      SCROLL_TICK=0
+                      DIRECTION='up'
                 if event.key == pygame.K_DOWN:
-                    DIRECTION = 'down'
-                    SCROLL_TICK=0
+                    if playerCollisions('down'):
+                      SCROLL_TICK=0
+                      DIRECTION='down'
                 if event.key == pygame.K_LEFT:
-                    DIRECTION = 'left'
-                    SCROLL_TICK=0
+                    if playerCollisions('left'):
+                      SCROLL_TICK=0
+                      DIRECTION='left'
                 if event.key == pygame.K_RIGHT:
-                    DIRECTION = 'right'
-                    SCROLL_TICK=0
+                    if playerCollisions('right'):
+                      SCROLL_TICK=0
+                      DIRECTION='right'
+                if event.key == pygame.K_p:
+                    map = maps.scenes['dev2']
             ##print(directions[DIRECTION][0])
             try:
                 #print(map[player_y-5][player_x-6])
@@ -139,7 +194,6 @@ while running:
                 pass
 
     ##print(player_x,"and",y)
-
     scroll(DIRECTION)
 
     screen.fill(WHITE)
@@ -148,21 +202,34 @@ while running:
     # Moving right shifts the map to left
     # Moving down shifts the map to up
 
+    
+    # screen drawing loops
     for y in range(-1, 12):
       for x in range(-1,14):
-        #print(player_x,player_y)
+        # check if x or y is not in the tile list
         if player_x+x-6>=0 and player_y+y-5>=0:
           try:
-            screen.blit(map[player_y+y-5][player_x+x-6], (x*32-offset[0], y*32-offset[1]))
+            # get current tile data
+            tile = map[player_y+y-5][player_x+x-6]
+            if isinstance(tile, list):
+              # draw tiles with extra properties
+              screen.blit(tileIDs[tile[0]], (x*32-offset[0], y*32-offset[1]))
+            else:
+              # draw tiles with no properties
+              screen.blit(tileIDs[tile], (x*32-offset[0], y*32-offset[1]))
+              
           except:
+            # draw water if indexerror
             screen.blit(water, (x*32-offset[0],y*32-offset[1]))
         else:
+          # draw water if x or y is less than 0
           screen.blit(water, (x*32-offset[0],y*32-offset[1]))
-          #print('hi')
 
     screen.blit(player, (192,160))
+    text(f'({player_x}, {player_y})', 5, 5, BLACK, 14, 'freesans')
+    text(f'{DIRECTION}', 50,5, BLACK, 15, 'freesans')
     #screen.blit(grass, (0,0))
 
-    pygame.display.flip()       
+    pygame.display.flip()   
 
 pygame.quit()
